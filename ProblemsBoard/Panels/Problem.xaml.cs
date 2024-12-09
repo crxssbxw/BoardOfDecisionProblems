6779ab3dcff48f6cc3e748261e3941b85f8f2c90
crxssbxw <ilya.afanas1ev@yandex.ru>
2024-12-09 12:23:27 +0300

    Main Problems List, panels for add and watch problems

---
+++ ProblemsBoard/Panels/Problem.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ProblemsBoard.Panels
+{
+    /// <summary>
+    /// Логика взаимодействия для Problem.xaml
+    /// </summary>
+    public partial class Problem : UserControl
+    {
+        public Problem()
+        {
+            InitializeComponent();
+        }
+        private void Close_Click(object sender, RoutedEventArgs e)
+        {
+            var Animation = new DoubleAnimation() { From = this.Opacity, To = 0, Duration = TimeSpan.FromSeconds(0.5) };
+            Animation.Completed += Animation_Completed;
+            this.BeginAnimation(OpacityProperty, Animation);
+        }
+        private void Animation_Completed(object? sender, EventArgs e)
+        {
+            Visibility = Visibility.Collapsed;
+        }
+    }
+}
