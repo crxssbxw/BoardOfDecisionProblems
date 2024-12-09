4d8f345d110c42042ee7a6d36e999620574db0e6
crxssbxw <ilya.afanas1ev@yandex.ru>
2024-12-09 12:23:27 +0300

    Main Problems List, panels for add and watch problems

---
+++ ProblemsBoard/Panels/NewProblem.xaml.cs
@@ -0,0 +1,51 @@
+﻿using ProblemsBoard.Windows;
+using ProblemsBoardLib;
+using ProblemsBoardLib.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ProblemsBoard.Panels
+{
+    /// <summary>
+    /// Логика взаимодействия для NewProblem.xaml
+    /// </summary>
+    public partial class NewProblem : UserControl
+    {
+        public NewProblem()
+        {
+            Animation.Completed += Animation_Completed;
+            InitializeComponent();
+        }
+
+        static DoubleAnimation Animation = new DoubleAnimation() { From = 0.9, To = 0, Duration = TimeSpan.FromSeconds(0.5) };
+        private void Close_Click(object sender, RoutedEventArgs e)
+        {
+            this.BeginAnimation(OpacityProperty, Animation);
+        }
+        private void Animation_Completed(object? sender, EventArgs e)
+        {
+            DataContext = null;
+            Visibility = Visibility.Collapsed;
+        }
+
+        private void Accept_Click(object sender, RoutedEventArgs e)
+        {
+            ProblemsBoardLib.Models.Problem? problem = DataContext as ProblemsBoardLib.Models.Problem;
+            MainWindow.problemsViewModel.Add(problem);
+            this.BeginAnimation(OpacityProperty, Animation);
+        }
+    }
+}
---
+++ ProblemsBoard/Windows/MainWindow.xaml.cs
@@ -0,0 +1,64 @@
+﻿using ProblemsBoardLib.ViewModel;
+using ProblemsBoardLib.Models;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ProblemsBoard.Windows;
+
+/// <summary>
+/// Interaction logic for MainWindow.xaml
+/// </summary>
+public partial class MainWindow : Window
+{
+    public static ProblemsViewModel problemsViewModel = new();
+    public MainWindow()
+    {
+        InitializeComponent();
+        DataContext = problemsViewModel;
+    }
+    private DoubleAnimation OpacityAnimation = new DoubleAnimation() { From = 0, To = 0.9, Duration = TimeSpan.FromSeconds(0.5) };
+    private void Statistic_Click(object sender, RoutedEventArgs e)
+    {
+        StatisticPanel.Visibility = Visibility.Visible;
+        StatisticPanel.BeginAnimation(OpacityProperty, OpacityAnimation);
+    }
+
+    private void NewProblem_Click(object sender, RoutedEventArgs e)
+    {
+        NewProblemPanel.Visibility = Visibility.Visible;
+        NewProblemPanel.BeginAnimation(OpacityProperty, OpacityAnimation);
+        Problem newProblem = new();
+        newProblem.Status = "Решается";
+        NewProblemPanel.DataContext = newProblem;
+    }
+
+    private int TestPlusCount = 1;
+    private void TestPlus_Click(object sender, RoutedEventArgs e)
+    {
+        string[] Statuses = { "Решено", "Решается" };
+        Problem problem = new()
+        {
+            ProblemId = TestPlusCount++,
+            Description = "Test",
+            Status = Statuses[new Random().Next(0, 2)]
+        };
+        problemsViewModel.Add(problem);
+    }
+
+    private void ProblemView_Click(object sender, RoutedEventArgs e)
+    {
+        ProblemPanel.Visibility = Visibility.Visible;
+        ProblemPanel.BeginAnimation(OpacityProperty, OpacityAnimation);
+        var DataSender = sender as Button;
+        ProblemPanel.DataContext = DataSender.DataContext;
+    }
+}
